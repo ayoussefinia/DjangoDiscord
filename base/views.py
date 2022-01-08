5b4f07ebafb3e9eb12a77b25e543fe14849330ac
Alex Youssefinia <alex.youssefinia@gmail.com>
2022-01-07 21:52:54 -0600

    added Create and Update functionality to the rooms model, also created templates for /create-room and /update-room

--- base/views.py
+++ base/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
  
-rooms = [
-    {'id': 1, 'name' : 'lets learn python'},
-    {'id': 2, 'name' : 'design with me'},
-    {'id': 3, 'name': 'frontend developers'}
-]
+# rooms = [
+#     {'id': 1, 'name' : 'lets learn python'},
+#     {'id': 2, 'name' : 'design with me'},
+#     {'id': 3, 'name': 'frontend developers'}
+# ]
 # Create your views here.
 def home(request): 
-    return render(request, './base/home.html', {'rooms': rooms})
+    rooms=Room.objects.all()
+    context={'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
-   
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        print(request.POST)
+    context = {'form': form}
+    return render(request, "base/room_form.html", context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
